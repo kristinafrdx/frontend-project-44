793b1e1dfe225747aca2d7992d4ef9355c684d3c
kristinafrdx <kristina.frdx@mail.ru>
2023-06-13 18:39:36 +0400

    exchange error

--- src/logicOfGames/game_IsPrime.js
+++ src/logicOfGames/game_IsPrime.js
@@ -1,9 +1,11 @@
 import { getRandome, startGame } from '../index.js';
 
 export const isPrime = (number) => {
-  for (let i = 2; i < number; i += 1) {
-    if (number % i === 0) {
-      return false;
+  if (number > 0) {
+    for (let i = 2; i < number; i += 1) {
+      if (number % i === 0) {
+        return false;
+      }
     }
   }
   return true;
