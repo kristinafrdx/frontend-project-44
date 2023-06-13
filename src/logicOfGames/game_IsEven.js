4a1a93f32d9def4edc94a40cc2471ddfd539642f
kristinafrdx <kristina.frdx@mail.ru>
2023-06-13 15:02:07 +0400

    return comments

--- src/logicOfGames/game_IsEven.js
+++ src/logicOfGames/game_IsEven.js
@@ -13,3 +13,31 @@ const gameIsEven = () => {
 export default () => {
   startGame(description, gameIsEven);
 };
+
+/* const game = () => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let count = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandome(1000, 0);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      count += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'`);
+      return `Let's try again ${nameUser}!`;
+    }
+  }
+  if (count === 3) {
+    return `Congratulation, ${nameUser}!`;
+  }
+};
+export default game;
+
+*/
--- src/logicOfGames/game_calc.js
+++ src/logicOfGames/game_calc.js
@@ -31,3 +31,36 @@ const gameCalc = () => {
 export default () => {
   startGame(description, gameCalc);
 };
+
+/* const gameCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+  console.log('What is the result of the expression?');
+
+  let count = 0;
+  for (let i = 0; i < 3; i += 1) {
+    const number1 = getRandome(20, 1);
+    const number2 = getRandome(20, 1);
+    const getRandomeIndex = Math.floor(Math.random() * arr.length);
+    const operand = arr[getRandomeIndex];
+
+    console.log(`Question: ${number1} ${operand} ${number2}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = calculat(number1, operand, number2);
+    if (Number(answer) === correctAnswer) {
+      count += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      return (`"Let's try again, ${nameUser}!"`);
+    }
+  }
+  if (count === 3) {
+    return `Congratulation, ${nameUser}!`;
+  }
+};
+
+export default gameCalc;
+
+*/
