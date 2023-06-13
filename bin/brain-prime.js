4ce756d46389f6e3c295f8698bc524b0a968bc55
kristinafrdx <kristina.frdx@mail.ru>
2023-06-13 13:20:44 +0400

    add game_isPrime

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import gamePrime from '../src/logicOfGames/game_IsPrime.js';
+
+gamePrime();
---
+++ src/logicOfGames/game_IsPrime.js
@@ -0,0 +1,23 @@
+import { getRandome, startGame } from '../index.js';
+
+export const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const gamePrime = () => {
+  const number = getRandome(30, 1);
+  const question = `${number}`;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+export default () => {
+  startGame(description, gamePrime);
+};
